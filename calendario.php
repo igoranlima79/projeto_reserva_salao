1a76adaf64b97a5356e2763370f9f2c559426026
Pedro-Dbr <pedroborgesdbr@gmail.com>
2024-11-27 15:09:22 -0300

    Update calendario.php

--- calendario.php
+++ calendario.php
@@ -1,1 +1,6 @@
 <?php
+function gerarCalendario($mes, $ano)
+  {
+    // Obter o numero do mês e o numero dde dias no mês
+$nomeMes = date("F", mktime(0, 0, 0, $mes,1 , $ano));
+    $diasNoMes = cal_days_in_month(call_GREGORIAN, $mes, $ano);
